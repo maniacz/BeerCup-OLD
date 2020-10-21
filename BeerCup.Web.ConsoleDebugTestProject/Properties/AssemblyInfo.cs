d77ecc6465c1993fe9a9502203ea6ba9305e4a8a
Łukasz K <lkipka@gmail.com>
2020-10-21 11:56:41 +0200

    Utworzenie projektów: BeerCup.Web.ConsoleDebug BeerCup.Web.ConsoleDebugTestProject

---
+++ BeerCup.Web.ConsoleDebugTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BeerCup.Web.ConsoleDebugTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BeerCup.Web.ConsoleDebugTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9befa41f-5435-4fb8-b6d1-e9a411d4c9ce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

4e8b67d0f480fe2d50493c2b82383ad925651b8e
Łukasz K <lkipka@gmail.com>
2020-10-27 14:20:43 +0100

    Utworzono klasę BeerCodingManager

---
+++ BeerCup/BeerCup/Data/BeerCodingManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerCup.Data
+{
+    public class BeerCodingManager : DbManager
+    {
+        internal void ConfirmBeerCodingForBattle(Dictionary<int, string> beerCoding)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

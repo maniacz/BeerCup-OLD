38388a4de042381f14a98a37288aee908b69e7cf
Łukasz K <lkipka@gmail.com>
2020-10-27 12:33:00 +0100

    Dodano klasę BeerCodingEntity

---
+++ BeerCup/BeerCup/Data/Entities/BeerCodingEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerCup.Data.Entities
+{
+    public class BeerCodingEntity
+    {
+        public int BeerNumber { get; set; }
+        public int BreweryId { get; set; }
+    }
+}

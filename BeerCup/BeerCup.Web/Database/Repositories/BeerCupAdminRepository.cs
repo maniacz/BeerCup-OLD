0fea553e940a7159d3b18729e5823f8e05256530
Łukasz K <lkipka@gmail.com>
2020-10-31 12:37:00 +0100

    Dodano klasę BeerCupAdminRepository oraz interfejs IBeerCupAdminRepository

---
+++ BeerCup/BeerCup.Web/Database/Repositories/BeerCupAdminRepository.cs
@@ -0,0 +1,18 @@
+﻿using BeerCup.Web.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeerCup.Web.Database.Repositories
+{
+    public class BeerCupAdminRepository : IBeerCupAdminRepository
+    {
+        private BeerCupContext dbContext = new BeerCupContext();
+
+        public List<Brewery> GetCurrentBattleBreweries()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

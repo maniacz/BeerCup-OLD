9fb260661636d284c265456921c14f305e57395a
Łukasz K <lkipka@gmail.com>
2020-11-04 11:44:38 +0100

    Dodano REST API metodę GET - GetCurrentBattleBreweries pobierającą listę browarów startujących w trwającej bitwie

--- BeerCup/BeerCup.Web/Controllers/BeerCupAdminController.cs
+++ BeerCup/BeerCup.Web/Controllers/BeerCupAdminController.cs
@@ -1,14 +1,43 @@
-﻿using System;
+﻿using BeerCup.Web.Database.Entities;
+using BeerCup.Web.Database.Repositories;
+using BeerCup.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace BeerCup.Web.Controllers
 {
     public class BeerCupAdminController : ApiController
     {
+        private IBeerCupAdminRepository beerCupAdminRepository;
+
+        public BeerCupAdminController(IBeerCupAdminRepository beerCupAdminRepository)
+        {
+            this.beerCupAdminRepository = beerCupAdminRepository;
+        }
+
+        [Route("api/currentbattlebreweries")]
+        [HttpGet]
+        public HttpResponseMessage GetCurrentBattleBreweries()
+        {
+            var response = new BeerCupAdminResponse();
+
+            try
+            {
+                response.CurrentBattleBreweries = beerCupAdminRepository.GetCurrentBattleBreweries();
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = ex.Message;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, response, Configuration.Formatters.JsonFormatter);
+        }
+
         // GET api/<controller>
         public IEnumerable<string> Get()
         {
---
+++ BeerCup/BeerCup.Web/Models/BeerCupAdminResponse.cs
@@ -0,0 +1,13 @@
+﻿using BeerCup.Web.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BeerCup.Web.Models
+{
+    public class BeerCupAdminResponse : BaseResponse
+    {
+        public List<Brewery> CurrentBattleBreweries { get; set; }
+    }
+}

4edb9f2a2ed6a6ee653aad704fe345bcc94d4f7e
Łukasz K <lkipka@gmail.com>
2020-12-12 15:21:12 +0100

    Dodano NuGet Autofac oraz zręb klasy AppContainer - zarządzającej kontenerem IoC z Autofaca

---
+++ BeerCup/BeerCup/Bootstrap/AppContainer.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BeerCup.ViewModels;
+
+namespace BeerCup.Bootstrap
+{
+    public class AppContainer
+    {
+        private static IContainer _container;
+
+        public static void RegisterDependencies()
+        {
+            var builder = new ContainerBuilder();
+
+            //ViewModels
+            builder.RegisterType<BaseViewModel>();
+            builder.RegisterType<VotingViewModel>();
+
+            //Services - Data
+
+        }
+    }
+}
